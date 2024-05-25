89e52a056dfd1d61edaa330a908f99e85f3ebf96
Ali Zhumatayev <alizhumataeyev@Alis-MacBook-Air-2.local>
2024-05-26 03:15:06 +0500

    level 1

--- nfactorialeve/event_app/models.py
+++ nfactorialeve/event_app/models.py
@@ -1,3 +1,14 @@
+
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    location = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    image_url = models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.title
+
